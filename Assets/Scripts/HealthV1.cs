48c6f5e544c6e5c110fafdbc4895b782a249b4f1
chanyutx10 <30559766+chanyutx10@users.noreply.github.com>
2026-02-13 10:27:13 +0700

    WIP

--- Assets/Scripts/HealthV1.cs
+++ Assets/Scripts/HealthV1.cs
@@ -2,8 +2,15 @@ using UnityEngine;
 
 public class HealthV1 : MonoBehaviour
 {
+    public int hp = 100;
+
     public void TakeDamage(int damage)
     {
-
+        //hp = hp - damage;
+        hp -= damage;
+        if (hp <= 0)
+        { 
+            Destroy(gameObject);
+        }
     }
 }
